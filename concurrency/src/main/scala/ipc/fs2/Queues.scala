e44a4eaa3f98f2f2b3d49ba19cf0291b28e5461d
yaneeve.shekel <yaneeve.shekel@perseus.de>
2018-06-16 22:16:45 +0200

    take one at fs2 conversation

--- concurrency/src/main/scala/ipc/fs2/Queues.scala
+++ concurrency/src/main/scala/ipc/fs2/Queues.scala
@@ -3,22 +3,27 @@ package ipc.fs2
 import cats.effect.{Effect, IO}
 import cats.effect.ConcurrentEffect._
 import cats.effect.implicits._
+import com.typesafe.scalalogging.LazyLogging
 import fs2.StreamApp.ExitCode
 import fs2.async.mutable.Queue
 import fs2.{Stream, StreamApp, async}
+import ipc.Text
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.language.higherKinds
 
-class Buffering(q1: Queue[IO, Int], q2: Queue[IO, Int])(implicit IO: Effect[IO]) {
+case class ConversationPartner(msgQueue: Queue[IO, String], name: String)
+class Interlocutor(/*q1: Queue[IO, String],*/name: String, conversationPartner: ConversationPartner)(implicit IO: Effect[IO]) extends LazyLogging {
 
-  def start: Stream[IO, Unit] =
+  def start(text: Array[String]): Stream[IO, Unit] =
     Stream(
-      Stream.range(0, 1000).covary[IO].to(q1.enqueue),
-      q1.dequeue.to(q2.enqueue),
+//      Stream.emits(text).covary[IO].to(q1.enqueue),
+//      q1.dequeue.to(q2.enqueue),
+      Stream.emits(text).covary[IO].to(conversationPartner.msgQueue.enqueue),
+//      q1.dequeue.to(q2.enqueue),
       //.map won't work here as you're trying to map a pure value with a side effect. Use `evalMap` instead.
-      q2.dequeue.evalMap(n => IO.delay(println(s"Pulling out $n from Queue #2")))
+      conversationPartner.msgQueue.dequeue.evalMap(line => IO.delay(logger.info(s"${conversationPartner.name} received [$line] from $name")))
     ).join(3)
 
 }
@@ -27,16 +32,26 @@ class Buffering(q1: Queue[IO, Int], q2: Queue[IO, Int])(implicit IO: Effect[IO])
 
 object Fifo extends StreamApp[IO] {
 
-  override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, ExitCode] =
+  override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, ExitCode] = {
+    def createConversationPartner(name: String): Stream[IO, ConversationPartner] = for {
+      msgQueue <- Stream.eval(async.boundedQueue[IO, String](1))
+      conversationPartner <- Stream.eval(IO.pure(ConversationPartner(msgQueue, name)))
+    } yield conversationPartner
+
     Stream.every[IO](500.millis).flatMap { implicit S =>
       for {
-        q1 <- Stream.eval(async.boundedQueue[IO, Int](1))
-        q2 <- Stream.eval(async.boundedQueue[IO, Int](100))
-        bp = new Buffering(q1, q2)
-        _ <- Stream.sleep[IO](3.seconds) concurrently bp.start.drain
+        beatrix <- createConversationPartner("btrx")
+        adam <- createConversationPartner("adam")
+        adamInterlocutor = new Interlocutor("adam", beatrix)
+        btrxInterlocutor = new Interlocutor("btrx", adam)
+        _ <- Stream.sleep[IO](2.seconds)
+        _ <- Stream.sleep[IO](3.seconds) concurrently
+          (adamInterlocutor.start(Text.adam).drain concurrently
+          btrxInterlocutor.start(Text.beatrix).drain)
         ec <- Stream.emit(ExitCode.Success).covary[IO]
       } yield ec
     }
+  }
 
 }
 
